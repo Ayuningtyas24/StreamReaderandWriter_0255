17bf7f840a414eecdd3fd92e863814ddaa0961d2
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-06-10 10:46:15 +0700

    menambahkan tampilan untuk membuka file dan menunjuk ke sebuah file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -32,4 +32,10 @@ int main()
     }
     // selesai dalam menulis sekarang tutup filenya
     outfile.close();
+
+    // membuka file dalam mode membaca
+    ifstream infile;
+
+    // menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
 }
