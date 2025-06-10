2952d9e6dcd1802f9b85177e3bb21922166b8bd8
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-06-10 10:39:24 +0700

    menambahkan deklarasi variabel dan file mode menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -2,3 +2,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke subuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+}
